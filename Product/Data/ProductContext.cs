2310622e63c5dd7ab230f7095b0d86c69648366a
hebaran <36790650+hebaran@users.noreply.github.com>
2026-05-02 12:22:29 -0300

    fix: Adicionando a tabela Produtos

--- Product/Data/ProductContext.cs
+++ Product/Data/ProductContext.cs
@@ -1,9 +1,11 @@
 using Microsoft.EntityFrameworkCore;
+using Product.Models;
 
 namespace Product.Data;
 
 public class ProductContext : DbContext
 {
+    public DbSet<ProductModel> Products { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlite("Data source=products.sqlite");
