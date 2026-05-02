5f899f5638441986bd6ac6a0fecb436f3ea6f159
hebaran <36790650+hebaran@users.noreply.github.com>
2026-05-02 13:07:00 -0300

    new: Criando as duas rotas principais do programa. (POST Product e GET All Products)

---
+++ Product/Routes/ProductRoute.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using Product.Data;
+using Product.Models;
+
+namespace Product.Routes;
+
+public static class ProductRoute
+{
+    public static void ProductRoutes(this WebApplication app)
+    {
+        var productsRoute = app.MapGroup("/products");
+
+        productsRoute.MapPost("/",
+        async (ProductRequest request, ProductContext context) =>
+        {
+            string productName = request.Name;
+            double productPrice = request.Price;
+            int productStock = request.Stock ?? 0;
+
+            var product = new ProductModel(productName, productPrice, productStock);
+            await context.AddAsync(product);
+            await context.SaveChangesAsync();
+
+            return Results.Created($"/products/{product.Id}", product);
+        });
+
+        productsRoute.MapGet("/",
+        async (ProductContext context) =>
+        {
+            var products = await context.Products.ToListAsync();
+            return Results.Ok(products);
+        });
+    }
+}
